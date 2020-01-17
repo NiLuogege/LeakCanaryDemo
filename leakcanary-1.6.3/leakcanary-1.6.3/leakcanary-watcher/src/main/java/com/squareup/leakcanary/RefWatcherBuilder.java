c3e312ebce49a831601f613b2bb886107a262300
luoChen <hank.luo@xianghuanji.com>
2020-01-17 14:07:46 +0800

    。。

--- leakcanary-1.6.3/leakcanary-1.6.3/leakcanary-watcher/src/main/java/com/squareup/leakcanary/RefWatcherBuilder.java
+++ leakcanary-1.6.3/leakcanary-1.6.3/leakcanary-watcher/src/main/java/com/squareup/leakcanary/RefWatcherBuilder.java
@@ -9,146 +9,162 @@ import java.util.List;
  */
 public class RefWatcherBuilder<T extends RefWatcherBuilder<T>> {
 
-  private HeapDump.Listener heapDumpListener;
-  private DebuggerControl debuggerControl;
-  private HeapDumper heapDumper;
-  private WatchExecutor watchExecutor;
-  private GcTrigger gcTrigger;
-  private final HeapDump.Builder heapDumpBuilder;
+    private HeapDump.Listener heapDumpListener;
+    private DebuggerControl debuggerControl;
+    private HeapDumper heapDumper;
+    private WatchExecutor watchExecutor;
+    private GcTrigger gcTrigger;
+    private final HeapDump.Builder heapDumpBuilder;
 
-  public RefWatcherBuilder() {
-    heapDumpBuilder = new HeapDump.Builder();
-  }
-
-  /** @see HeapDump.Listener */
-  public final T heapDumpListener(HeapDump.Listener heapDumpListener) {
-    this.heapDumpListener = heapDumpListener;
-    return self();
-  }
-
-  /** @see ExcludedRefs */
-  public final T excludedRefs(ExcludedRefs excludedRefs) {
-    heapDumpBuilder.excludedRefs(excludedRefs);
-    return self();
-  }
-
-  /** @see HeapDumper */
-  public final T heapDumper(HeapDumper heapDumper) {
-    this.heapDumper = heapDumper;
-    return self();
-  }
-
-  /** @see DebuggerControl */
-  public final T debuggerControl(DebuggerControl debuggerControl) {
-    this.debuggerControl = debuggerControl;
-    return self();
-  }
-
-  /** @see WatchExecutor */
-  public final T watchExecutor(WatchExecutor watchExecutor) {
-    this.watchExecutor = watchExecutor;
-    return self();
-  }
-
-  /** @see GcTrigger */
-  public final T gcTrigger(GcTrigger gcTrigger) {
-    this.gcTrigger = gcTrigger;
-    return self();
-  }
-
-  /** @see Reachability.Inspector */
-  public final T stethoscopeClasses(
-      List<Class<? extends Reachability.Inspector>> stethoscopeClasses) {
-    heapDumpBuilder.reachabilityInspectorClasses(stethoscopeClasses);
-    return self();
-  }
-
-  /**
-   * Whether LeakCanary should compute the retained heap size when a leak is detected. False by
-   * default, because computing the retained heap size takes a long time.
-   */
-  public final T computeRetainedHeapSize(boolean computeRetainedHeapSize) {
-    heapDumpBuilder.computeRetainedHeapSize(computeRetainedHeapSize);
-    return self();
-  }
-
-  /** Creates a {@link RefWatcher}. */
-  public final RefWatcher build() {
-    if (isDisabled()) {
-      return RefWatcher.DISABLED;
+    public RefWatcherBuilder() {
+        heapDumpBuilder = new HeapDump.Builder();
     }
 
-    if (heapDumpBuilder.excludedRefs == null) {
-      heapDumpBuilder.excludedRefs(defaultExcludedRefs());
+    /**
+     * @see HeapDump.Listener
+     */
+    public final T heapDumpListener(HeapDump.Listener heapDumpListener) {
+        this.heapDumpListener = heapDumpListener;
+        return self();
     }
 
-    HeapDump.Listener heapDumpListener = this.heapDumpListener;
-    if (heapDumpListener == null) {
-      heapDumpListener = defaultHeapDumpListener();
+    /**
+     * @see ExcludedRefs
+     */
+    public final T excludedRefs(ExcludedRefs excludedRefs) {
+        heapDumpBuilder.excludedRefs(excludedRefs);
+        return self();
     }
 
-    DebuggerControl debuggerControl = this.debuggerControl;
-    if (debuggerControl == null) {
-      debuggerControl = defaultDebuggerControl();
+    /**
+     * @see HeapDumper
+     */
+    public final T heapDumper(HeapDumper heapDumper) {
+        this.heapDumper = heapDumper;
+        return self();
     }
 
-    HeapDumper heapDumper = this.heapDumper;
-    if (heapDumper == null) {
-      heapDumper = defaultHeapDumper();
+    /**
+     * @see DebuggerControl
+     */
+    public final T debuggerControl(DebuggerControl debuggerControl) {
+        this.debuggerControl = debuggerControl;
+        return self();
     }
 
-    WatchExecutor watchExecutor = this.watchExecutor;
-    if (watchExecutor == null) {
-      watchExecutor = defaultWatchExecutor();
+    /**
+     * @see WatchExecutor
+     */
+    public final T watchExecutor(WatchExecutor watchExecutor) {
+        this.watchExecutor = watchExecutor;
+        return self();
     }
 
-    GcTrigger gcTrigger = this.gcTrigger;
-    if (gcTrigger == null) {
-      gcTrigger = defaultGcTrigger();
+    /**
+     * @see GcTrigger
+     */
+    public final T gcTrigger(GcTrigger gcTrigger) {
+        this.gcTrigger = gcTrigger;
+        return self();
     }
 
-    if (heapDumpBuilder.reachabilityInspectorClasses == null) {
-      heapDumpBuilder.reachabilityInspectorClasses(defaultReachabilityInspectorClasses());
+    /**
+     * @see Reachability.Inspector
+     */
+    public final T stethoscopeClasses(
+            List<Class<? extends Reachability.Inspector>> stethoscopeClasses) {
+        heapDumpBuilder.reachabilityInspectorClasses(stethoscopeClasses);
+        return self();
     }
 
-    return new RefWatcher(watchExecutor, debuggerControl, gcTrigger, heapDumper, heapDumpListener,
-        heapDumpBuilder);
-  }
+    /**
+     * Whether LeakCanary should compute the retained heap size when a leak is detected. False by
+     * default, because computing the retained heap size takes a long time.
+     */
+    public final T computeRetainedHeapSize(boolean computeRetainedHeapSize) {
+        heapDumpBuilder.computeRetainedHeapSize(computeRetainedHeapSize);
+        return self();
+    }
 
-  protected boolean isDisabled() {
-    return false;
-  }
+    /**
+     * Creates a {@link RefWatcher}.
+     */
+    public final RefWatcher build() {
+        if (isDisabled()) {
+            return RefWatcher.DISABLED;
+        }
 
-  protected GcTrigger defaultGcTrigger() {
-    return GcTrigger.DEFAULT;
-  }
+        if (heapDumpBuilder.excludedRefs == null) {
+            heapDumpBuilder.excludedRefs(defaultExcludedRefs());
+        }
 
-  protected DebuggerControl defaultDebuggerControl() {
-    return DebuggerControl.NONE;
-  }
+        HeapDump.Listener heapDumpListener = this.heapDumpListener;
+        if (heapDumpListener == null) {
+            heapDumpListener = defaultHeapDumpListener();
+        }
 
-  protected ExcludedRefs defaultExcludedRefs() {
-    return ExcludedRefs.builder().build();
-  }
+        DebuggerControl debuggerControl = this.debuggerControl;
+        if (debuggerControl == null) {
+            debuggerControl = defaultDebuggerControl();
+        }
 
-  protected HeapDumper defaultHeapDumper() {
-    return HeapDumper.NONE;
-  }
+        HeapDumper heapDumper = this.heapDumper;
+        if (heapDumper == null) {
+            heapDumper = defaultHeapDumper();
+        }
 
-  protected HeapDump.Listener defaultHeapDumpListener() {
-    return HeapDump.Listener.NONE;
-  }
+        WatchExecutor watchExecutor = this.watchExecutor;
+        if (watchExecutor == null) {
+            watchExecutor = defaultWatchExecutor();
+        }
 
-  protected WatchExecutor defaultWatchExecutor() {
-    return WatchExecutor.NONE;
-  }
+        GcTrigger gcTrigger = this.gcTrigger;
+        if (gcTrigger == null) {
+            gcTrigger = defaultGcTrigger();
+        }
 
-  protected List<Class<? extends Reachability.Inspector>> defaultReachabilityInspectorClasses() {
-    return Collections.emptyList();
-  }
+        if (heapDumpBuilder.reachabilityInspectorClasses == null) {
+            heapDumpBuilder.reachabilityInspectorClasses(defaultReachabilityInspectorClasses());
+        }
 
-  @SuppressWarnings("unchecked")
-  protected final T self() {
-    return (T) this;
-  }
+        return new RefWatcher(watchExecutor, debuggerControl, gcTrigger, heapDumper, heapDumpListener,
+                heapDumpBuilder);
+    }
+
+    protected boolean isDisabled() {
+        return false;
+    }
+
+    protected GcTrigger defaultGcTrigger() {
+        return GcTrigger.DEFAULT;
+    }
+
+    protected DebuggerControl defaultDebuggerControl() {
+        return DebuggerControl.NONE;
+    }
+
+    protected ExcludedRefs defaultExcludedRefs() {
+        return ExcludedRefs.builder().build();
+    }
+
+    protected HeapDumper defaultHeapDumper() {
+        return HeapDumper.NONE;
+    }
+
+    protected HeapDump.Listener defaultHeapDumpListener() {
+        return HeapDump.Listener.NONE;
+    }
+
+    protected WatchExecutor defaultWatchExecutor() {
+        return WatchExecutor.NONE;
+    }
+
+    protected List<Class<? extends Reachability.Inspector>> defaultReachabilityInspectorClasses() {
+        return Collections.emptyList();
+    }
+
+    @SuppressWarnings("unchecked")
+    protected final T self() {
+        return (T) this;
+    }
 }
